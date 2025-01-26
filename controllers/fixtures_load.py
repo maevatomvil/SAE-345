41c6dca24e06efb00b89976d5e4f28d4c12c41ce
Kylian-VADEBOIN <kylian.vadeboin@edu.univ-fcomte.fr>
2025-01-26 15:38:23 +0100

    update fixtures_load.py

--- controllers/fixtures_load.py
+++ controllers/fixtures_load.py
@@ -10,7 +10,7 @@ fixtures_load = Blueprint('fixtures_load', __name__, template_folder='templates'
 @fixtures_load.route('/base/init')
 def fct_fixtures_load():
     mycursor = get_db().cursor()
-    file = open('sql_projet.sql', 'r')
+    file = open('sae_sql.sql', 'r')
     sql = file.read().replace(',\n', ',').replace('VALUES\n', 'VALUES').replace('(\n', '(').replace('    ', ' ').replace(')\n', ')').split('\n')
     file.close()
     sql = [i for i in sql if i != '']
