ff1b563b8264cca67398b238f5582f821fb1a7b9
Kylian-VADEBOIN <kylian.vadeboin@edu.univ-fcomte.fr>
2025-02-26 15:25:23 +0100

    Update client_commande.py

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -36,39 +36,59 @@ def client_commande_valide():
 def client_commande_add():
     mycursor = get_db().cursor()
 
-    # choix de(s) (l')adresse(s)
-
     id_client = session['id_user']
-    sql = '''selection du contenu du panier de l'utilisateur'''
-    items_ligne_panier = []
-    # if items_ligne_panier is None or len(items_ligne_panier) < 1:
-    #     flash(u'Pas d\'telephones dans le ligne_panier', 'alert-warning')
-    #     return redirect('/client/telephone/show')
-                                           # https://pynative.com/python-mysql-transaction-management-using-commit-rollback/
-    #a = datetime.strptime('my date', "%b %d %Y %H:%M")
 
-    sql = ''' creation de la commande '''
-
+    sql = "SELECT * FROM ligne_panier WHERE utilisateur_id = %s"
+    mycursor.execute(sql, id_client)
+    items_ligne_panier = mycursor.fetchall()
+    if items_ligne_panier is None or len(items_ligne_panier) < 1:
+        flash(u'Pas de télephones dans le panier', 'alert-warning')
+        return redirect('/client/telephone/show')
+    
+    date_commande = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    tuple_insert = (date_commande, id_client, '1')
+    sql = "INSERT INTO commande(date_achat, utilisateur_id, etat_id) VALUES (%s, %s, %s)"
+    mycursor.execute(sql, tuple_insert)
     sql = '''SELECT last_insert_id() as last_insert_id'''
-    # numéro de la dernière commande
+    mycursor.execute(sql)
+    commande_id = mycursor.fetchone()
+    print(commande_id, tuple_insert)
+
     for item in items_ligne_panier:
-        sql = ''' suppression d'une ligne de panier '''
-        sql = "  ajout d'une ligne de commande'"
+        sql = "DELETE FROM ligne_panier WHERE utilisateur_id = %s AND telephone_id = %s"
+        mycursor.execute(sql, (item['utilisateur_id'], item['telephone_id']))
+        sql = "SELECT prix_telephone AS prix FROM telephone WHERE id_telephone = %s"
+        mycursor.execute(sql, item['telephone_id'])
+        prix = mycursor.fetchone()
+        print(prix)
+        sql = "INSERT INTO ligne_commande(commande_id, telephone_id, prix, quantite) VALUES (%s, %s, %s, %s)"
+        tuple_insert = (commande_id['last_insert_id'], item['telephone_id'], prix['prix'], item['quantite'])
+        print(tuple_insert)
+        mycursor.execute(sql, tuple_insert)
 
     get_db().commit()
     flash(u'Commande ajoutée','alert-success')
     return redirect('/client/telephone/show')
 
-
-
-
 @client_commande.route('/client/commande/show', methods=['get','post'])
 def client_commande_show():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    sql = '''  selection des commandes ordonnées par état puis par date d'achat descendant '''
-    commandes = []
 
+    mycursor.execute("SELECT * FROM ligne_commande")
+    print(mycursor.fetchall())
+
+    sql = """SELECT commande.date_achat, COUNT(ligne_commande.telephone_id) AS nbr_telephones, SUM(ligne_commande.prix * ligne_commande.quantite) AS prix_total, commande.etat_id, etat.libelle 
+             FROM commande
+             JOIN ligne_commande ON commande.id_commande = ligne_commande.commande_id
+             JOIN etat ON commande.etat_id = etat.id_etat
+             WHERE utilisateur_id = %s
+             GROUP BY commande.date_achat, commande.etat_id, etat.libelle
+             ORDER BY commande.date_achat;"""
+    mycursor.execute(sql, id_client)
+    commandes = mycursor.fetchall()
+
+    print(commandes)
     telephones_commande = None
     commande_adresses = None
     id_commande = request.args.get('id_commande', None)
