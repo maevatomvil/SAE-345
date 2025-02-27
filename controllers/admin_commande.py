612fbe4f88de64e2c380c2731b8af5ea1e7b59bb
benj-ruy <benjamin.ruyer@edu.univ-fcomte.fr>
2025-02-27 18:18:37 +0100

    Affiche des commandes pour le vendeur

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -18,31 +18,35 @@ def admin_index():
 def admin_commande_show():
     mycursor = get_db().cursor()
     admin_id = session['id_user']
-    sql = '''      '''
+    sql = '''SELECT commande.id_commande, commande.date_achat, 
+             utilisateur.login, COUNT(ligne_commande.telephone_id) AS nbr_telephones, 
+             SUM(ligne_commande.prix * ligne_commande.quantite) AS prix_total, 
+             commande.etat_id, etat.libelle 
+             FROM commande
+             JOIN ligne_commande ON commande.id_commande = ligne_commande.commande_id
+             JOIN etat ON commande.etat_id = etat.id_etat
+             JOIN utilisateur ON commande.utilisateur_id = utilisateur.id_utilisateur
+             GROUP BY commande.id_commande, commande.date_achat, commande.utilisateur_id, 
+                     utilisateur.login, commande.etat_id, etat.libelle
+             ORDER BY commande.date_achat DESC;'''
 
-    commandes=[]
+    mycursor.execute(sql)
+    commandes = mycursor.fetchall()
 
     articles_commande = None
     commande_adresses = None
     id_commande = request.args.get('id_commande', None)
     print(id_commande)
     if id_commande != None:
-        sql = '''    '''
-        commande_adresses = []
+        sql = '''SELECT ligne_commande.*, telephone.nom_telephone AS nom, 
+                 (ligne_commande.prix * ligne_commande.quantite) AS prix_ligne  
+                 FROM ligne_commande
+                 JOIN telephone ON ligne_commande.telephone_id = telephone.id_telephone
+                 WHERE ligne_commande.commande_id = %s'''
+        mycursor.execute(sql, id_commande)
+        articles_commande = mycursor.fetchall()
     return render_template('admin/commandes/show.html'
                            , commandes=commandes
                            , articles_commande=articles_commande
                            , commande_adresses=commande_adresses
                            )
-
-
-@admin_commande.route('/admin/commande/valider', methods=['get','post'])
-def admin_commande_valider():
-    mycursor = get_db().cursor()
-    commande_id = request.form.get('id_commande', None)
-    if commande_id != None:
-        print(commande_id)
-        sql = '''           '''
-        mycursor.execute(sql, commande_id)
-        get_db().commit()
-    return redirect('/admin/commande/show')
