5f04e996294eb95517e5f7b6362c405aa00aed94
benj-ruy <benjamin.ruyer@edu.univ-fcomte.fr>
2025-02-27 21:17:08 +0100

    Détails des commandes pour l'admin

--- controllers/admin_commande.py
+++ controllers/admin_commande.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-from flask import Flask, request, render_template, redirect, flash, session
-
+from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+from datetime import datetime
 from connexion_db import get_db
 
 admin_commande = Blueprint('admin_commande', __name__,
-                        template_folder='templates')
+                           template_folder='templates')
+
 
 @admin_commande.route('/admin')
 @admin_commande.route('/admin/commande/index')
@@ -18,6 +19,7 @@ def admin_index():
 def admin_commande_show():
     mycursor = get_db().cursor()
     admin_id = session['id_user']
+
     sql = '''SELECT commande.id_commande, commande.date_achat, 
              utilisateur.login, COUNT(ligne_commande.telephone_id) AS nbr_telephones, 
              SUM(ligne_commande.prix * ligne_commande.quantite) AS prix_total, 
@@ -33,65 +35,72 @@ def admin_commande_show():
     mycursor.execute(sql)
     commandes = mycursor.fetchall()
 
-    articles_commande = None
+    telephones_commande = None
     commande_adresses = None
+    adresse_livraison = None
+    adresse_facturation = None
+
     id_commande = request.args.get('id_commande', None)
-    print(id_commande)
-    if id_commande != None:
-        sql = '''SELECT ligne_commande.*, telephone.nom_telephone AS nom, 
-                 (ligne_commande.prix * ligne_commande.quantite) AS prix_ligne  
+
+    if id_commande is not None:
+        sql_telephones = '''SELECT ligne_commande.*, telephone.nom_telephone AS nom, 
+                 (ligne_commande.prix * ligne_commande.quantite) AS prix_ligne,
+                 commande.id_commande AS commande_id,
+                 commande.etat_id AS etat_id  
                  FROM ligne_commande
                  JOIN telephone ON ligne_commande.telephone_id = telephone.id_telephone
+                 JOIN commande ON ligne_commande.commande_id = commande.id_commande
                  WHERE ligne_commande.commande_id = %s'''
-        mycursor.execute(sql, id_commande)
-        articles_commande = mycursor.fetchall()
+        mycursor.execute(sql_telephones, (id_commande,))
+        telephones_commande = mycursor.fetchall()
 
-        sql_adresse_livraison = '''SELECT commande.adresse_livraison_id, adresse.rue AS rue_livraison, 
-                          adresse.code_postal AS code_postal_livraison, adresse.ville AS ville_livraison,
-                          adresse.nom AS nom_livraison
-                   FROM commande
-                   JOIN adresse ON commande.adresse_livraison_id = adresse.id_adresse
-                   WHERE commande.id_commande = %s'''
+        sql_adresse_livraison = '''SELECT adresse.rue AS rue_livraison, 
+                                   adresse.code_postal AS code_postal_livraison, 
+                                   adresse.ville AS ville_livraison
+                                   FROM commande
+                                   JOIN adresse ON commande.adresse_livraison_id = adresse.id_adresse
+                                   WHERE commande.id_commande = %s'''
+        mycursor.execute(sql_adresse_livraison, (id_commande,))
+        adresse_livraison = mycursor.fetchone()
 
-        sql_adresse_facturation = '''SELECT commande.adresse_facturation_id, adresse.rue AS rue_facturation, 
-                            adresse.code_postal AS code_postal_facturation, adresse.ville AS ville_facturation,
-                            adresse.nom AS nom_facturation  
-                     FROM commande
-                     JOIN adresse ON commande.adresse_facturation_id = adresse.id_adresse
-                     WHERE commande.id_commande = %s'''
+        sql_adresse_facturation = '''SELECT adresse.rue AS rue_facturation, 
+                                     adresse.code_postal AS code_postal_facturation, 
+                                     adresse.ville AS ville_facturation
+                                     FROM commande
+                                     JOIN adresse ON commande.adresse_facturation_id = adresse.id_adresse
+                                     WHERE commande.id_commande = %s'''
+        mycursor.execute(sql_adresse_facturation, (id_commande,))
+        adresse_facturation = mycursor.fetchone()
 
-        try:
-            mycursor.execute(sql_adresse_livraison, id_commande)
-            adresse_livraison = mycursor.fetchone() or {}
-
-            mycursor.execute(sql_adresse_facturation, id_commande)
-            adresse_facturation = mycursor.fetchone() or {}
-
-            commande_adresses = {}
-            if adresse_livraison:
-                commande_adresses.update(adresse_livraison)
-            if adresse_facturation:
-                commande_adresses.update(adresse_facturation)
-
-        except Exception as e:
-            print(f"Erreur lors de la récupération des adresses: {e}")
-            commande_adresses = {}
+        commande_adresses = {}
+        if adresse_livraison:
+            commande_adresses.update({
+                'rue_livraison': adresse_livraison['rue_livraison'],
+                'code_postal_livraison': adresse_livraison['code_postal_livraison'],
+                'ville_livraison': adresse_livraison['ville_livraison']
+            })
+        if adresse_facturation:
+            commande_adresses.update({
+                'rue_facturation': adresse_facturation['rue_facturation'],
+                'code_postal_facturation': adresse_facturation['code_postal_facturation'],
+                'ville_facturation': adresse_facturation['ville_facturation']
+            })
 
     return render_template('admin/commandes/show.html'
                            , commandes=commandes
-                           , articles_commande=articles_commande
+                           , telephones_commande=telephones_commande
                            , commande_adresses=commande_adresses
                            )
 
-@admin_commande.route('/admin/commande/valider', methods=['get','post'])
+
+@admin_commande.route('/admin/commande/valider', methods=['get', 'post'])
 def admin_commande_valider():
     mycursor = get_db().cursor()
     commande_id = request.form.get('id_commande', None)
-    if commande_id != None:
-        print(commande_id)
+    if commande_id is not None:
         # Mise à jour de l'état de la commande vers "expédié" (id_etat=2)
         sql = '''UPDATE commande SET etat_id = 2 WHERE id_commande = %s'''
-        mycursor.execute(sql, commande_id)
+        mycursor.execute(sql, (commande_id,))
         get_db().commit()
         flash(u'Commande validée et expédiée', 'alert-success')
     return redirect('/admin/commande/show')
