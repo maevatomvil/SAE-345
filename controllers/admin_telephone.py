84e7c66640bb3269fecd796167ff2e3c93c43365
benj-ruy <benjamin.ruyer@edu.univ-fcomte.fr>
2025-02-28 17:36:08 +0100

    Changement de l'interface admin pour les téléphones + correction de fautes d'ortho

--- controllers/admin_telephone.py
+++ controllers/admin_telephone.py
@@ -18,13 +18,21 @@ admin_telephone = Blueprint('admin_telephone', __name__,
 def show_telephone():
     mycursor = get_db().cursor()
 
-    # Récupérer les paramètres de filtrage
     filter_stock = request.args.get('filter_stock')
 
     sql = '''
-    SELECT telephone.id_telephone, telephone.nom_telephone as nom, 
-           telephone.type_telephone_id, telephone.prix_telephone as prix,
-           telephone.stock, telephone.image, type_telephone.libelle_type_telephone as libelle
+    SELECT 
+        telephone.id_telephone, 
+        telephone.nom_telephone as nom, 
+        telephone.type_telephone_id, 
+        telephone.prix_telephone as prix,
+        COALESCE(telephone.poids, 'N/A') as poids,
+        COALESCE(telephone.taille, 'N/A') as taille,
+        COALESCE(telephone.fournisseur, 'N/A') as fournisseur,
+        COALESCE(telephone.marque, 'N/A') as marque,
+        telephone.stock, 
+        telephone.image, 
+        type_telephone.libelle_type_telephone as libelle
     FROM telephone
     LEFT JOIN type_telephone ON telephone.type_telephone_id = type_telephone.id_type_telephone
     '''
@@ -66,30 +74,47 @@ def valid_add_telephone():
     nom = request.form.get('nom', '')
     type_telephone_id = request.form.get('type_telephone_id', '')
     prix = request.form.get('prix', '')
-    fournisseur = request.form.get('fournisseur', '')  # Récupérer la valeur de 'fournisseur'
-    marque = request.form.get('marque', '')  # Récupérer la valeur de 'marque'
+    fournisseur = request.form.get('fournisseur', '')
+    marque = request.form.get('marque', '')
+    poids = request.form.get('poids', None)
+    taille = request.form.get('taille', None)
+    stock = request.form.get('stock', 0)
     image = request.files.get('image', '')
 
     if image:
         filename = 'img_upload' + str(int(2147483647 * random())) + '.png'
         image.save(os.path.join('static/images/', filename))
     else:
-        print("erreur")
         filename = None
 
-    # Mise à jour de la requête SQL pour inclure 'fournisseur' et 'marque'
-    sql = '''INSERT INTO telephone(nom_telephone, image, prix_telephone, type_telephone_id, fournisseur, marque) 
-             VALUES (%s, %s, %s, %s, %s, %s)'''
+    sql = '''INSERT INTO telephone(
+                nom_telephone, 
+                image, 
+                prix_telephone, 
+                type_telephone_id, 
+                fournisseur, 
+                marque, 
+                poids, 
+                taille, 
+                stock
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
 
-    tuple_add = (nom, filename, prix, type_telephone_id, fournisseur, marque)
+    tuple_add = (
+        nom,
+        filename,
+        prix,
+        type_telephone_id,
+        fournisseur,
+        marque,
+        poids,
+        taille,
+        stock
+    )
 
-    print(tuple_add)
     mycursor.execute(sql, tuple_add)
     get_db().commit()
 
-    print(u'téléphone ajouté , nom: ', nom, ' - type_telephone:', type_telephone_id, ' - prix:', prix,
-          ' - fournisseur:', fournisseur, ' - marque:', marque, ' - image:', image)
-    message = u'téléphone ajouté , nom:' + nom + '- type_telephone:' + type_telephone_id + ' - prix:' + prix + ' - fournisseur:' + fournisseur + ' - marque:' + marque + ' - image:' + str(image)
+    message = f'Téléphone ajouté : {nom} - Type: {type_telephone_id} - Prix: {prix} - Fournisseur: {fournisseur} - Marque: {marque}'
     flash(message, 'alert-success')
 
     return redirect('/admin/telephone/show')
@@ -157,11 +182,21 @@ def delete_telephone():
 def edit_telephone():
     id_telephone = request.args.get('id_telephone')
     mycursor = get_db().cursor()
+
+    # Updated SQL to fetch all new columns
     sql = '''
-    SELECT telephone.id_telephone, telephone.nom_telephone as nom, 
-           telephone.prix_telephone as prix, telephone.type_telephone_id, 
-           telephone.image, telephone.stock,
-           type_telephone.libelle_type_telephone as libelle
+    SELECT 
+        telephone.id_telephone, 
+        telephone.nom_telephone as nom, 
+        telephone.prix_telephone as prix, 
+        telephone.type_telephone_id, 
+        telephone.image, 
+        telephone.stock,
+        telephone.poids,
+        telephone.taille,
+        telephone.fournisseur,
+        telephone.marque,
+        type_telephone.libelle_type_telephone as libelle
     FROM telephone
     LEFT JOIN type_telephone ON telephone.type_telephone_id = type_telephone.id_type_telephone
     WHERE telephone.id_telephone = %s
@@ -169,6 +204,7 @@ def edit_telephone():
     mycursor.execute(sql, (id_telephone,))
     telephone = mycursor.fetchone()
 
+    # Fetch types of telephones
     sql = '''
     SELECT id_type_telephone, libelle_type_telephone as libelle
     FROM type_telephone
@@ -185,47 +221,71 @@ def edit_telephone():
 @admin_telephone.route('/admin/telephone/edit', methods=['POST'])
 def valid_edit_telephone():
     mycursor = get_db().cursor()
-    nom = request.form.get('nom')
-    id_telephone = request.form.get('id_telephone')
-    image = request.files.get('image', '')
-    type_telephone_id = request.form.get('type_telephone_id', '')
-    prix = request.form.get('prix', '')
 
-    sql = '''
-    SELECT image FROM telephone WHERE id_telephone = %s
-    '''
+    # Collect form data
+    id_telephone = request.form.get('id_telephone')
+    nom = request.form.get('nom')
+    type_telephone_id = request.form.get('type_telephone_id')
+    prix = request.form.get('prix')
+    fournisseur = request.form.get('fournisseur')
+    marque = request.form.get('marque')
+    poids = request.form.get('poids', None)
+    taille = request.form.get('taille', None)
+    stock = request.form.get('stock')
+    image = request.files.get('image', '')
+
+    # Fetch existing image name
+    sql = '''SELECT image FROM telephone WHERE id_telephone = %s'''
     mycursor.execute(sql, (id_telephone,))
     image_nom = mycursor.fetchone()
     image_nom = image_nom['image']
 
+    # Handle image upload
     if image:
-        if image_nom != "" and image_nom is not None and os.path.exists(
-                os.path.join(os.getcwd() + "/static/images/", image_nom)):
-            os.remove(os.path.join(os.getcwd() + "/static/images/", image_nom))
+        # Remove old image if exists
+        if image_nom and image_nom != "" and os.path.exists(os.path.join(os.getcwd(), "static/images/", image_nom)):
+            os.remove(os.path.join(os.getcwd(), "static/images/", image_nom))
 
-        # Génération d'un nom de fichier unique: préfixe + nombre aléatoire (max 2^31-1) + extension .png
-        # Pour éviter les collisions entre fichiers et standardiser le format des images
+        # Generate unique filename
         filename = 'img_upload_' + str(int(2147483647 * random())) + '.png'
         image.save(os.path.join('static/images/', filename))
         image_nom = filename
 
+    # Update SQL
     sql = '''
     UPDATE telephone 
-    SET nom_telephone = %s, 
+    SET 
+        nom_telephone = %s, 
         image = %s, 
         prix_telephone = %s, 
-        type_telephone_id = %s 
+        type_telephone_id = %s,
+        fournisseur = %s,
+        marque = %s,
+        poids = %s,
+        taille = %s,
+        stock = %s
     WHERE id_telephone = %s
     '''
-    mycursor.execute(sql, (nom, image_nom, prix, type_telephone_id, id_telephone))
+
+    mycursor.execute(sql, (
+        nom,
+        image_nom,
+        prix,
+        type_telephone_id,
+        fournisseur,
+        marque,
+        poids,
+        taille,
+        stock,
+        id_telephone
+    ))
 
     get_db().commit()
 
-    if image_nom is None:
-        image_nom = ''
-
-    message = u'telephone modifié , nom:' + nom + '- type_telephone :' + type_telephone_id + ' - prix:' + prix + ' - image:' + image_nom
+    # Prepare flash message
+    message = f'Téléphone modifié : {nom} - Type: {type_telephone_id} - Prix: {prix}'
     flash(message, 'alert-success')
+
     return redirect('/admin/telephone/show')
 
 
